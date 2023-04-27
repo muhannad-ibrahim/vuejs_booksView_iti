0f3040d34f56dd9b16b29051aefe2371dff37f7e
muhannad ibrahim <muhannad.ibrahim2311@gmail.com>
2023-04-27 18:27:54 +0200

    adding books view

---
+++ src/books.js
@@ -0,0 +1,83 @@
+export default [
+    {
+      isbn: "9781593275846",
+      name: "Eloquent JavaScript",
+      category: "Programming",
+      image: `https://edit.org/images/cat/book-covers-big-2019101610.jpg`,
+      pages: 40,
+      author: "Marijn Haverbeke",
+      price: 30
+    },
+    {
+      isbn: "9780061120084",
+      name: "To Kill a Mockingbird",
+      category: "Fiction",
+      image: `https://www.adobe.com/express/create/cover/media_178ebed46ae02d6f3284c7886e9b28c5bb9046a02.jpeg?width=400&format=jpeg&optimize=medium`,
+      pages: 336,
+      author: "Harper Lee",
+      price: 12
+    },
+    {
+      isbn: "9780670020553",
+      name: "The Help",
+      category: "Fiction",
+      image: `https://template.canva.com/EADaopxBna4/1/0/251w-ujD6UPGa9hw.jpg`,
+      pages: 45,
+      author: "Kathryn Stockett",
+      price: 16
+    },
+    {
+      isbn: "9780345391803",
+      name: "Jurassic Park",
+      category: "Science Fiction",
+      image: `https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-business-leadership-book-cover-design-template-dce2f5568638ad4643ccb9e725e5d6ff.jpg?ts=1637017516`,
+      pages: 448,
+      author: "Michael Crichton",
+      price: 9
+    },
+    {
+      isbn: "9780571357503",
+      name: "Hamnet",
+      category: "Historical Fiction",
+      image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9U2yleepTKWum2O_4i3rOhoFNJVFnuvWgXQ&usqp=CAU`,
+      pages: 20,
+      author: "Maggie O'Farrell",
+      price: 25
+    },
+    {
+      isbn: "9780316346627",
+      name: "The Martian",
+      category: "Science Fiction",
+      image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9U2yleepTKWum2O_4i3rOhoFNJVFnuvWgXQ&usqp=CAU`,
+      pages: 400,
+      author: "Andy Weir",
+      price: 14
+    },
+    {
+      isbn: "9780062569331",
+      name: "The Alchemist",
+      category: "Fiction",
+      image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9U2yleepTKWum2O_4i3rOhoFNJVFnuvWgXQ&usqp=CAU`,
+      pages: 10,
+      author: "Paulo Coelho",
+      price: 11
+    },
+    {
+      isbn: "9780062692415",
+      name: "Circe",
+      category: "Fantasy",
+      image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9U2yleepTKWum2O_4i3rOhoFNJVFnuvWgXQ&usqp=CAU`,
+      pages: 400,
+      author: "Madeline Miller",
+      price: 17
+    },
+    {
+      isbn: "9780743273565",
+      name: "The Great Gatsby",
+      category: "Fiction",
+      image: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9U2yleepTKWum2O_4i3rOhoFNJVFnuvWgXQ&usqp=CAU`,
+      pages: 180,
+      author: "F. Scott Fitzgerald",
+      price: 8.50
+    }
+  ];
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="d-flex justify-content-between align-items baseline p-2 my-2 bg-dark text-light">
+    <a href="#" class="m-3" style="text-decoration: none; color:orange; font-size: 1.5rem;"
+      @click="isBooksVisible = true">Books</a>
+    <div class="d-flex align-items-baseline justify-content-between">
+      <p class="mx-2">{{ list.items.length }} <template v-if="list.items.length == 1">item</template> <template
+          v-else>items</template> | Total price [ {{ formatPrice(getTotalPrice()) }} ]</p>
+      <button class="btn btn-primary m-3" @click="isBooksVisible = false">Show List</button>
+    </div>
   </div>
+  <!-- end of navbar-->
+
+
+  <!--main content-->
+  <div class="row m-auto text-center" v-if="isBooksVisible == true">
+    <div class="card border m-2" style="width: 25rem;" v-for="book in books">
+      <img :src="book.image" :alt="book.title">
+      <div class="card-header" :title="book.author">
+        {{ book.name }}
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+          <div class="d-flex align-items-baseline justify-content-between">
+            <p class="mx-2">{{ book.category }}</p>
+            <p class="mx-2">{{ book.author }}</p>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="d-flex align-items-baseline justify-content-between">
+            <p :class="[book.pages > 50 ? 'more' : 'less']" class="mx-2">Total pages: {{ book.pages }}</p>
+            <p class="mx-2">Price: {{ formatPrice(book.price) }}</p>
+          </div>
+        </li>
+        <li class="list-group-item">
+          <div class="d-flex align-items-baseline justify-content-between">
+            <p class="mx-2">{{ book.isbn }}</p>
+            <button href="#" class="btn btn-primary" @click="addToList(book)" :disabled="itemExistsInList(book)">Add To
+              List</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="row text-center" v-if="isBooksVisible == false">
+    <h3 class="text-danger" v-if="list.items.length == 0">
+      There's no books to show in the list ...
+    </h3>
+    <table class="table table-striped table-bordered text-center" v-else>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Pages</th>
+          <th>Author</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in list.items">
+          <td>{{ item.book.name }}</td>
+          <td>{{ item.book.pages }}</td>
+          <td>{{ item.book.author }}</td>
+          <td>
+            <button class="btn btn-danger" @click="removeItem">Remove</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <!--end of main content-->
+
+
+  <!--footer-->
+  <div class="row">
+    <div class="col-12 bg-dark text-light text-center p-3">
+      <h6>Online Shopping app</h6>
+      <p>2021</p>
+    </div>
+  </div>
+  <!--end of footer-->
 </template>
 
 <script>
+import books from '../books';
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: () => ({
+    books: books,
+    isBooksVisible: true,
+    list: {
+      items: []
+    }
+  }),
+  methods: {
+    addToList(book) {
+      this.list.items.push({
+        book: book,
+      });
+    },
+    getTotalPrice() {
+      let total = 0;
+      this.list.items.forEach(item => {
+        total += item.book.price;
+      });
+      return total;
+    },
+    itemExistsInList(book) {
+      return this.list.items.find(item => item.book.isbn === book.isbn);
+    },
+    removeItem(item) {
+      this.list.items.splice(this.list.items.indexOf(item), 1);
+    },
+    formatPrice(price) {
+      return Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0 }).format(price);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+img {
+  height: 35rem;
+  object-fit: cover;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+ul>li>div>p {
+  padding: 0.6rem;
+  font-size: 1.2rem;
+  border-radius: 0.5rem;
+  background-color: lightgrey;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.more {
+  background-color: lightgreen;
 }
-a {
-  color: #42b983;
+
+.less {
+  background-color: lightcoral;
 }
 </style>
